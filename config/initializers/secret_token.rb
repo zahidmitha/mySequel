8b773e90ab09e5b1fd3d05153a7d827fd38a306e
Leo Allen <leo.f.allen@gmail.com>
2013-05-23 14:18:57 +0100

    Added rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MySequel::Application.config.secret_token = 'ab1a2ea7a90cd7ba1e5e264479dc9a86f0eb0055f89c1cdba4992662c09367dbdf11a392bc2517dd401f272dc9c19a3cab2125fc5c306ed1710fc2d35988ad49'
